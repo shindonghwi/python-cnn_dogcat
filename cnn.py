2e680ef226361cb6e6fa1e3fb5d718c888650407
shindonghwi <ehdgnl8940@naver.com>
2022-09-13 18:15:52 +0900

    feat: cnn 네트워크 구축

--- cnn.py
+++ cnn.py
@@ -1,4 +1,5 @@
 """ 1. Import Library """
+import keras
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -6,7 +7,6 @@ print(tf.__version__)
 print(tf.config.list_physical_devices())
 
 """ 2. Data Preprocessing"""
-
 # Training data set preprocessing
 # 훈련 과적합을 방지하기 위해 훈련 데이터넷을 전처리한다.
 # * 과적합: 훈련 데이터셋에서는 정확도가 아주 높게 나오지만, 테스트 데이터셋에서는 정확도가 훨씬 낮은 현상
@@ -35,3 +35,29 @@ test_set = test_dategen.flow_from_directory(
     batch_size=32,
     class_mode='binary'
 )
+
+""" 3. Building CNN """
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+
+# cnn init
+cnn = Sequential()
+
+# 1 convolution & pooling ( max )
+cnn.add(Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+cnn.add(MaxPool2D(pool_size=(2, 2), strides=2))
+
+# 2 convolution & pooling ( max )
+cnn.add(Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(MaxPool2D(pool_size=(2, 2), strides=2))
+
+# 3 Flattening
+cnn.add(Flatten())
+
+# 4 Full Connection
+cnn.add(Dense(units=128, activation='relu'))
+
+# 5 Output Layer
+# 이중 분류(개,고양이) 에서는 시그모이드 활성화 함수를 권장
+# 다중 분류에서는 소프트맥스 활성화 함수 사용 ( 각 예측 확률의 합을 1로 만들어야하기에 )
+cnn.add(Dense(units=1, activation='sigmoid'))
