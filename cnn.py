29f2bc8691883dfb4dd3b07096580a8adbee23b9
shindonghwi <ehdgnl8940@naver.com>
2022-09-13 17:52:09 +0900

    feat: 훈련,테스트 데이터 셋을 전처리하고 불러오는 방법

--- cnn.py
+++ cnn.py
@@ -1,7 +1,37 @@
-
 """ 1. Import Library """
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
+
 print(tf.__version__)
 print(tf.config.list_physical_devices())
 
+""" 2. Data Preprocessing"""
+
+# Training data set preprocessing
+# 훈련 과적합을 방지하기 위해 훈련 데이터넷을 전처리한다.
+# * 과적합: 훈련 데이터셋에서는 정확도가 아주 높게 나오지만, 테스트 데이터셋에서는 정확도가 훨씬 낮은 현상
+train_dategen = ImageDataGenerator(
+    rescale=1. / 255,  # 픽셀 값을 255로 나누어서 스케일링 적용
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+training_set = train_dategen.flow_from_directory(
+    directory='dataset/training_set',
+    target_size=(64, 64),  # 이미지의 최종 크기 - 컨벌루션의 입력
+    batch_size=32,
+    class_mode='binary'
+)
+
+# Test data set preprocessing
+test_dategen = ImageDataGenerator(
+    rescale=1. / 255
+)  # 테스트 이미지는 원본처럼 그대로 유지하여야한다. ( 새로들어온 이미지가 변형이 안되어 있을 거기에! )
+
+test_set = test_dategen.flow_from_directory(
+    directory='dataset/test_set',
+    target_size=(64, 64),  # 이미지의 최종 크기 - 컨벌루션의 입력
+    batch_size=32,
+    class_mode='binary'
+)
